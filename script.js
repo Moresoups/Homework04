e966413edc925b89aef59c7ea099b5851b4b8753
Moresop <gardner_christopher@wheatoncollege.edu>
2022-10-23 16:36:03 -0400

    its is sloppy but done, gif stuck in html

--- script.js
+++ script.js
@@ -1,44 +1,39 @@
-var questions = [{
+let questions = [{
     q: "Commonly used data types DO NOT include:",
     a: "1. Strings",
     b: "2. Booleans",
     c: "3. Alerts",
     d: "4. Numbers",
     correct: "3. Alerts",
-},
-{
+}, {
     q: "The condition in an if/else statement is enclosed with ____.",
     a: "1. Quotes",
     b: "2. Curly brackets",
     c: "3. Parentheses",
     d: "4. Square brackets",
     correct: "3. Parentheses",
-},
-{
+}, {
     q: "Arrays in JavaScript can be used to store ____.",
     a: "1. Numbers and strings",
     b: "2. Other arrays",
     c: "3. Booleans",
     d: "4. All of the above",
     correct: "4. All of the above",
-},
-{
-    q: "String values must be encosed within ____ when being assigned to variables.",
+}, {
+    q: "String values must be encosed within ____ when being assigned to letiables.",
     a: "1. Quotes",
     b: "2. Curly brackets",
     c: "3. Parentheses",
     d: "4. Square brackets",
     correct: "1. Quotes",
-},
-{
+}, {
     q: "A very useful tool used during development and debugging for printing content to the debugger is:",
     a: "1. Javascript",
     b: "2. Terminal/Bash",
     c: "3. for loops",
     d: "4. console.log",
     correct: "4. console.log",
-},
-{
+}, {
     q: "What is the correct syntax for referring to an external script called 'code.js'?",
     a: "1. <script src='code.js'>",
     b: "2. <script href='code.js'>",
@@ -47,14 +42,17 @@ var questions = [{
     correct: "1. <script src='code.js'>",
 }];
 
-var clickStart = document.getElementById("start");
-var timerEl = document.getElementById("countdown");
-var timeLeft = 60;
-var quizDuration;
+//these questions are all just copied from the prompt, I didn't make any of them up on my own
+
+let clickStart = document.getElementById("start");
+let timerEl = document.getElementById("countdown");
+let timeLeft = 60;
+let quizDuration;
+let questionContainer = document.querySelector("#quiz-container");
 
 function timer() {
-    timerEl.textContent = "Time remaining: " + timeLeft + "seconds";
-    quizDuration = setInterval(function () {
+    timerEl.textContent = "Time remaining: " + timeLeft + " seconds";
+    quizDuration = setInterval(function() {
         if (timeLeft > 0) {
             adjustTime(-1);
         } else {
@@ -62,33 +60,35 @@ function timer() {
         }
     }, 1000);
 }
+
 function adjustTime(amount) {
     timeLeft += amount;
     if (timeLeft < 0) {
         timeLeft = 0;
     }
-    timerEl.textContent = "Time remaining: " + timeLeft + "seconds";
+    timerEl.textContent = "Time remaining: " + timeLeft + " seconds";
 }
+
 clickStart.onclick = timer;
-var renderQuestion = function (question) {
+let renderQuestion = function(question) {
     questionContainer.innerHTML = "";
 
-    var questionHeader = document.createElement("h2");
+    let questionHeader = document.createElement("h2");
     questionHeader.textContent = question.q;
 
-    var answerA = document.createElement("button");
+    let answerA = document.createElement("button");
     answerA.textContent = question.a;
     answerA.addEventListener("click", answerClick);
 
-    var answerB = document.createElement("button");
+    let answerB = document.createElement("button");
     answerB.textContent = question.b;
     answerB.addEventListener("click", answerClick);
 
-    var answerC = document.createElement("button");
+    let answerC = document.createElement("button");
     answerC.textContent = question.c;
     answerC.addEventListener("click", answerClick);
 
-    var answerD = document.createElement("button");
+    let answerD = document.createElement("button");
     answerD.textContent = question.d;
     answerD.addEventListener("click", answerClick);
 
@@ -99,46 +99,50 @@ var renderQuestion = function (question) {
     questionContainer.appendChild(answerD);
 }
 
-var currentQuestionIndex = 0;
-var userScore = 0;
-var correctAnswer = questions[currentQuestionIndex].correct;
-var clickViewScores = document.getElementById("view-score");
+let currentQuestionIndex = 0;
+let userScore = 0;
+let correctAnswer = questions[currentQuestionIndex].correct;
+let clickViewScores = document.getElementById("view-score");
 
-var answerClick = function(event) {
+let answerClick = function(event) {
     event.preventDefault();
-    var userAnswer = event.target.textContent;
+    let userAnswer = event.target.textContent;
     correctAnswer = questions[currentQuestionIndex].correct;
-  
-    var answerDetermination = document.querySelector("#answer-determination");
+
+    let answerDetermination = document.querySelector("#answer-determination");
     if (userAnswer !== correctAnswer) {
         adjustTime(-10);
-        answerDetermination.textContent = "Wrong! dumbass!";
+        answerDetermination.textContent = "lol dumbass you got it wrong";
         currentQuestionIndex++;
         if (currentQuestionIndex >= questions.length) {
             endQuizPage();
-        } else {renderQuestion(questions[currentQuestionIndex])};
+        } else {
+            renderQuestion(questions[currentQuestionIndex])
+        };
 
-    }
-    else if (userAnswer === correctAnswer) {
+    } else if (userAnswer === correctAnswer) {
         currentQuestionIndex++;
         answerDetermination.textContent = "Correct!";
         userScore++;
         if (currentQuestionIndex >= questions.length) {
             endQuizPage();
-        } else {renderQuestion(questions[currentQuestionIndex])};
+        } else {
+            renderQuestion(questions[currentQuestionIndex])
+        };
     }
 };
 
-var quiz = function (event) {
+let quiz = function(event) {
     event.preventDefault();
     resetDisplay();
     renderQuestion(questions[currentQuestionIndex]);
 };
 
 function resetDisplay() {
-    questionContainer.innerHTML="";
+    questionContainer.innerHTML = "";
     document.querySelector("#intro-page").style.display = "none";
 }
+
 function highScores() {
     let data = localStorage.getItem("object");
     let getData = JSON.parse(data);
@@ -147,57 +151,64 @@ function highScores() {
     questionContainer.innerHTML = "";
     questionContainer.innerHTML = name + " " + score;
 }
+clickViewScores.addEventListener("click", () => {
+    highScores();
+})
 
-var initials; 
+let initials;
+
 function endQuizPage() {
     resetDisplay();
     timerEl.textContent = "";
     clearInterval(quizDuration);
-    var endPage = document.createElement("h2");
+    let endPage = document.createElement("h2");
     questionContainer.appendChild(endPage);
 
     let blank = document.querySelector("#answer-determination");
     blank.innerHTML = "";
 
-    endPage.innerHTML = "your final score is" + userScore;
+    endPage.innerHTML = "All done! Your final score is " + userScore + ". Enter your initials to save";
 
-    var initialBox = document.createElement("input");
+    let initialBox = document.createElement("input");
     blank.appendChild(initialBox);
 
-    var submitInitialBtn = document.createElement("button");
+    let submitInitialBtn = document.createElement("button");
     submitInitialBtn.textContent = "Submit";
     blank.appendChild(submitInitialBtn);
-        
+
+    submitInitialBtn.addEventListener("click", () => {
+
+
         if (initialBox.value.length === 0) return false;
 
         let storeInitials = (...input) => {
-            let data = JSON.stringify({ "name":input[0], "score":input[1]})
+            let data = JSON.stringify({
+                "name": input[0],
+                "score": input[1]
+            })
             localStorage.setItem("object", data)
         }
         storeInitials(initialBox.value, userScore);
 
-        var playAgain = document.createElement("button");
-        playAgain.textContent= "Play Again!";
+        let playAgain = document.createElement("button");
+        playAgain.textContent = "Play Again!";
         blank.appendChild(playAgain);
 
-        playAgain.addEventListener"click", () => {
+        playAgain.addEventListener("click", () => {
             location.reload();
-        }
-    };
+        })
+    });
 
     document.querySelector("input").value = "";
 
     initialBox.addEventListener("submit", endQuizPage);
-    
+
+};
+
 function renderInitials() {
     submitInitialBtn.addEventListener('click', function(event) {
         event.preventDefault;
-}
-)};
+    })
+};
 
 clickStart.addEventListener('click', quiz);
-
-function myFunction() {
-    var element = document.body;
-    element.classList.toggle("dark-mode");
-  }
